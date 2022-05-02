4826f3e96ea7afa25e802048ce026c9af3780c34
Redshore <isutare412@gmail.com>
2022-05-02 23:11:07 +0900

    Hide session detail from MongoDB

--- gateway/pkg/port/repository.go
+++ gateway/pkg/port/repository.go
@@ -4,15 +4,9 @@ import (
 	"context"
 
 	centity "github.com/isutare412/hexago/common/pkg/entity"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Sessional interface {
-	StartSession(context.Context) (mongo.SessionContext, error)
-}
-
 type UserRepository interface {
-	Sessional
 	InsertUser(ctx context.Context, user *centity.User) error
 	FindUserById(ctx context.Context, id string) (*centity.User, error)
 	DeleteUserById(ctx context.Context, id string) error
